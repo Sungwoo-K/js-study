684f65eb86921bed87b7a180c5613b79ccd36b87
unknown <swkim960715@gmail.com>
2023-07-10 17:53:46 +0900

    Learn js

--- 01-core/script.js
+++ 01-core/script.js
@@ -1,3 +1,26 @@
-const mainWord = document.querySelector("p");
+function checkAge(age) {
+  return age > 18 ? true : confirm('보호자의 동의를 받으셨나요?');
+}
 
-mainWord.innerText = "hello world";
+function checkAge2(age) {
+  return (age > 18) || confirm('보호자의 동의를 받으셨나요?');
+}
+
+let age = 11;
+checkAge(age);
+checkAge2(age);
+
+const min = (a, b) => {
+  return a <= b ? a : b;
+}
+
+alert(min(11, 12));
+
+const pow = (x, n) => {
+  return x ** n;
+}
+
+const a = prompt("숫자를 입력해주세요");
+const b = prompt("몇 제곱 입니까?");
+
+alert(pow(a, b));
